09379e327ff14adf0a954bb07a852f680778e242
dev2033 <dev.elop.02@mail.ru>
2020-10-26 18:00:09 +0300

    update downloader

--- y2b_downloader.py
+++ y2b_downloader.py
@@ -4,25 +4,7 @@ import os
 import pafy
 from p2d_logo import logo
 
-# url = ""
-#
-# v = pafy.new(url)
-# print(v)
-# print(v.title)
-# print(v.duration)
-# print(v.viewcount)
 
-# streams = v.streams
-# for item in streams:
-#     print(item)
-
-# audio_streams = v.audiostreams
-# for item in audio_streams:
-#     print(item)
-
-# all_streams = v.allstreams
-# for item in all_streams:
-#     print(item.quality)
 print(logo)
 print("Хотите скачать видео или аудио с YouTube? Просто введите URL ниже...")
 url = input("Введите URL: ")
@@ -41,7 +23,8 @@ def download(choice):
         else:
             sys.exit()
 
-        print("Выберите желаемое качество видеоролика передав цифру. Пример: 1: ") if choice == "1" else print("Выберите желаемое качество аудио передав цифру. Пример: 2: ")
+        print("Выберите желаемое качество видеоролика передав цифру. Пример: 1 ")
+        if choice == "1" else print("Выберите желаемое качество аудио передав цифру. Пример: 2 ")
 
         available_streams = {}
         count = 1
@@ -49,7 +32,6 @@ def download(choice):
             available_streams[count] = stream
             print(f"{count}: {stream}")
             count += 1
-        # print(available_streams)
 
         stream_count = int(input("Введите номер: "))
         d = streams[stream_count - 1].download()
@@ -70,48 +52,3 @@ def download(choice):
 
 download(choice)
 
-
-# if choice == "1":
-#     try:
-#         v = pafy.new(url)
-#         # print(v.title)
-#
-#         print("Выберите желаемое качество видеоролика передав цифру. Пример: 1: ")
-#
-#         available_streams = {}
-#         count = 1
-#         video_streams = v.streams
-#         for stream in video_streams:
-#             available_streams[count] = stream
-#             print(f"{count}: {stream}")
-#             count += 1
-#         # print(available_streams)
-#
-#         stream_count = int(input("Введите номер: "))
-#         d = video_streams[stream_count - 1].download()
-#         print("Скачивание успешно завершено!")
-#     except:
-#         print("Упс...Проверьте данные")
-# elif choice == "2":
-#     try:
-#         v = pafy.new(url)
-#         # print(v.title)
-#
-#         print("Выберите желаемое качество аудио передав цифру. Пример: 1: ")
-#
-#         available_streams = {}
-#         count = 1
-#         video_streams = v.audiostreams
-#         for stream in video_streams:
-#             available_streams[count] = stream
-#             print(f"{count}: {stream}")
-#             count += 1
-#         # print(available_streams)
-#
-#         stream_count = int(input("Введите номер: "))
-#         d = video_streams[stream_count - 1].download()
-#         print("Скачивание успешно завершено!")
-#     except:
-#         print("Упс...Проверьте данные")
-# else:
-#     print("Whaaat???")
